c27bb16a713a552c72117e2f3faab834b001a4e7
martin.cuba <martin.cuba459@gmail.com>
2022-06-13 23:35:53 -0300

    isolate how to write and read a file in a shared class

---
+++ Sat.Recruitment.Src/Shared/Shared.Infrastructure/Persistence/File/IFilePersistence.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Infrastructure.Persistence.File
+{
+    public interface IFilePersistence
+    {
+        Task WriteLine(string filePath, bool appendData, string line);
+
+        Task<List<string>> Read(string filePath);
+    }
+}
